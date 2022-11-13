3efe0cb650eafeebf14309ce70655d869c869e05
Alex Top1055 <123alexfeetham@gmail.com>
2022-11-13 16:06:40 +0000

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,20 +1,23 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Video_Cutter
+namespace Updater
 {
-    internal static class Program
+    static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new VidoCutter());
+            Application.Run(new Updater());
         }
     }
 }
--- README.md
+++
@@ -1,4 +0,0 @@
-
-# Video Cutter
-
-A video trimmer and compressor mainly for discord users trying to post videos under file size limitations
